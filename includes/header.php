14c44a7318ddca970869c8c4c39e319e27a22784
MiralChauhan <miralchauhan28@gmail.com>
2024-02-21 16:06:36 +0530

    preloader and login path changed

--- includes/header.php
+++ includes/header.php
@@ -34,7 +34,7 @@
 
         <!-- Preloader -->
         <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__shake" src="dist/img/logo.jpeg" alt="AdminLTELogo" height="60" width="60">
+            <img class="animation__shake" src="../dist/img/logo.jpeg" alt="AdminLTELogo" height="60" width="60">
         </div>
 
         <!-- Navbar -->
