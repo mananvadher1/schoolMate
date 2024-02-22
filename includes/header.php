60f38bfb14cab67133d6a33cd0e945d2a0f4e591
Khushi1692 <kathrotiyakhushi@gmail.com>
2024-02-22 13:01:23 +0530

    manage role data submite into db

--- includes/header.php
+++ includes/header.php
@@ -36,9 +36,9 @@
     <div class="wrapper">
 
         <!-- Preloader -->
-        <!-- <div class="preloader flex-column justify-content-center align-items-center">
+        <div class="preloader flex-column justify-content-center align-items-center">
             <img class="animation__shake" src="../dist/img/logo.jpeg" alt="AdminLTELogo" height="60" width="60">
-        </div> -->
+        </div>
 
         <!-- Navbar -->
         <nav class="main-header navbar navbar-expand navbar-white navbar-light bg-dark fixed-top">
--- pages/manage_role.php
+++ pages/manage_role.php
@@ -1,10 +1,56 @@
 <?php 
+session_start();
 include("../includes/db.php");
 
 include("../includes/header.php");
   
 include("../includes/sidebar.php");
+
+if($_SERVER['REQUEST_METHOD']=='POST') {
+  $role_name=$_POST["role_name"];
+  $email=$_SESSION["email"];
+
+  $sql = "INSERT INTO `roles` ( `role_name`, `created_by`, `created_dt`, `updated_by`, `updated_dt`) VALUES ( '$role_name', '$email', CURRENT_TIMESTAMP(), NULL, NULL);";
+
+$result = mysqli_query($conn, $sql);
+
+if ($result) {
+  //echo "New record created successfully";
+  $insert=true;
+} 
+else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+}
+// echo var_dump($_SESSION);
 ?>
+<div class="card card-secondary ">
+    <div class="card-header">
+        <div class="row">
+            <div class="col">
+                <h3 class="card-title my-2">Manage Roles</h3>
+            </div>
+            <div class="col-auto">
+                <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button"
+                    aria-expanded="false" aria-controls="collapseExample">
+                    Add Role
+                </a>
+            </div>
+        </div>
+    </div>
+    <div >
+        <div class="collapse mt-3" id="collapseExample">
+            <form class="form-inline" method="post">
+                <div class="form-group mx-sm-3 mb-2">
+                    <label class="form-label mx-2" for="role_name">Enter Role Name</label>
+                    <input type="text" class="form-control" id="role_name" name="role_name" placeholder="Enter Role Name">
+                </div>
+                <button type="submit" class="btn btn-primary mb-2">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
 
 
 <?php 
