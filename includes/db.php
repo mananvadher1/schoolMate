00f2d9d13d5ed55ca2c4bfc2189d478960759058
mananvadher1 <mananvadher1@gmail.com>
2024-03-13 16:15:12 +0530

    check loggedin in db.php

--- includes/db.php
+++ includes/db.php
@@ -1,12 +1,16 @@
 <?php
 session_start();
 
-$conn = mysqli_connect("localhost","phpmyadmin","Admin@123","SchoolMate");
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] != true) {
+    
+    header("location: http://localhost/schoolMate/login.php");
+    exit;
+}
+
+$conn = mysqli_connect("localhost", "phpmyadmin", "Admin@123", "SchoolMate");
 //  if($conn){
 //     echo "success";
 //  }
-if(mysqli_connect_error()){
-    echo"can not connect";
+if (mysqli_connect_error()) {
+    echo "can not connect";
 }
-
-?>
