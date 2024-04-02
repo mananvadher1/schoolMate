2c4f747d398c80d08da2c983a7377d39af074cd5
Khushi1692 <kathrotiyakhushi@gmail.com>
2024-04-02 15:19:49 +0530

    give shape to p tag

--- pages/attendance.php
+++ pages/attendance.php
@@ -1,22 +1,22 @@
 <?php include("../controller/attendance_control.php"); ?>
 <style>
     .heading {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-}
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
 </style>
 <div class="container">
     <div class="heading my-4">
-        <h1><b>Attendance - Class <?php echo $_SESSION['class_id']?></b></h1>
+        <h1><b>Attendance - Class <?php echo $_SESSION['class_id'] ?></b></h1>
         <button type="button" class="btn btn-success"><i class="fa fa-download" aria-hidden="true"></i>&nbsp;&nbsp;Download</button>
     </div>
     <div class="date my-4">
-    <div class="date-wrapper">
-        <h2>Date:<?php echo date("j-n-y"); ?></h2>
+        <div class="date-wrapper">
+            <h2>Date:<?php echo date("j-n-y"); ?></h2>
+        </div>
     </div>
-</div>
-<table class="table" id="myTable">
+    <table class="table" id="myTable">
         <thead>
             <tr>
                 <th scope="col">Roll No</th>
@@ -26,21 +26,23 @@
             </tr>
         </thead>
         <tbody>
-                <?php $sno=0;
-                while ($row_dt = mysqli_fetch_assoc($re_dt)) {
-                    $sno = $sno + 1;
-                    echo "<tr id=" . $row_dt['id'] . ">
+            <?php $sno = 0;
+            while ($row_dt = mysqli_fetch_assoc($re_dt)) {
+                $sno = $sno + 1;
+                echo "<tr id=" . $row_dt['id'] . ">
                     <td>" . $sno . "</td>
-                    <td>" . $row_dt['first_name'] ."  ".$row_dt['last_name'] . "</td>
+                    <td>" . $row_dt['first_name'] . "  " . $row_dt['last_name'] . "</td>
                     <td><button onClick='editClick(" . $row_dt['id'] . ")' class='present btn btn-sm btn-success'>P</button>
                     <button onClick='deleteClick(" . $row_dt['id'] . ")' class='abesent btn btn-sm btn-danger'>A</button>
-                    <button type='button' id='bigpresent'class='btn btn-success'>Present</button>
-                    <button type='button'id='bigabsent' class='btn btn-danger'>Absent</button></td>
+                    <p id='bigpresent' class='d-inline-block' style='background-color: green; color: white; padding: 5px 10px; margin-right: 10px; border-radius: 3px;'>Present</p>
+                    <p id='bigabsent' class='d-inline-block' style='background-color: red; color: white; padding: 5px 10px; border-radius: 3px;'>Absent</p>
+                    
+</td>
                 </tr>";
-                } ?>
-            </tbody>
+            } ?>
+        </tbody>
 
-</table>
+    </table>
 
 </div>
 
