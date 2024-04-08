50a59f3d9d0a11d40dbba524cf380dd232947ab2
MiralChauhan <miralchauhan28@gmail.com>
2024-04-08 16:52:17 +0530

    change password page

--- controller/setting_control.php
+++
@@ -1,5 +0,0 @@
-<?php
-include("../includes/db.php");
-include("../includes/header.php");
-include("../includes/sidebar.php");
-?>
--- pages/settings.php
+++ pages/settings.php
@@ -1,5 +1,18 @@
-<?php include ("../controller/setting_control.php"); ?>
+<?php 
+include("../includes/db.php");
+include("../includes/header.php");
+include("../includes/sidebar.php");
+?>
 
-
+<div class="container my-3">
+    <div class="heading">
+        <h1><b>Settings</b></h1>
+    </div>
+    <div class="content my-3">
+        <a href="change_password.php"><button type="button" class="btn btn-outline-primary btn-lg btn-block my-3">Change Your Account Password</button></a>
+        <a href="change_password.php"><button type="button" class="btn btn-outline-primary btn-lg btn-block my-3">Change Your Account Theme</button></a>
+        <a href="change_password.php"><button type="button" class="btn btn-outline-primary btn-lg btn-block my-3">Change Your Account Wallpaper</button></a>
+    </div>
+</div>
 
 <?php include ("../includes/footer.php"); ?>
